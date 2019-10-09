8618bf8084927ea9f1cbeba5e3bf541a7662588f
lauren <ljdutton2@icloud.com>
2019-10-08 18:54:02 -0700

    big issues

--- app.py
+++ app.py
@@ -1,20 +1,73 @@
 from flask import Flask, render_template, request
 import requests
+from bson.objectid import ObjectId
 from pymongo import MongoClient
 import os
+from pprint import pprint
 
-client = MongoClient()
+
+
+host = os.environ.get('MONGODB_URI', 'mongodb://127.0.0.1:27017/Contractor')
+client = MongoClient(host=host)
 db = client.Contractor
 products = db.products
+
+
+
 app = Flask(__name__)
+FLASK_APP = app
+
+images = [
+ "http://cdn.shopify.com/s/files/1/0038/9405/0868/products/Houseplants_SpiderPlant_Bonnie_6in_1024x1024.jpg?v=1552591517",
+'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIgg5hzQ3VuR68NZCqyTkSXpfz19BF5Z4Bs9ZxmBBX9Svfx2N7vA&s',
+'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSS7wiQCWofg7KKLvQNU-4ujGoTVBYHUQ-KUyzHCSyWE_Lup-Wn&s',
+'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY7bXTlRvTj5RWE1u9sTDE3Y4TVI_YeJ3S07KDIBpGeF4XtFra&s',
+'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6pW5rc--yKDwkD0q3xFiI3R0o2NrK9kUg32SHBUuqvbCG6YGw&s'
+]
+plants = [
+ {"name": "Spider Plant",
+ "picture": images[0],
+ "price": "3.99",
+ "difficulty": "Easy"},  
+ {"name": "Yellow Tulip",
+ "picture": images[1],
+ "price": "8.99",
+ "difficulty": "hard"},  
+ {"name": "Assorted Succulents",
+ "picture": images[2],
+ "price": "1.99",
+ "difficulty": "Ez"},  
+ {"name": "Sunflowers",
+ "picture": images[3],
+ "price": "10.00",
+ "difficulty": "Medium"},  
+ {"name": "Bonsai Tree",
+ "picture": images[0],
+ "price": "12.99",
+ "difficulty": "Very Difficult"},   
+]
+
 
 @app.route('/')
-def index():
-    return("TEST")
+def show_home():
+    return render_template('plants_index.html')
 
-@app.route('/home')
+
+@app.route('/view')
 def get_store():
-        return render_template('index.html')
+    for plant in plants:
+        products.insert_many(plant)
+    return render_template('all_products.html', products=products.find())
+
+
+@app.route('/products/<product_id>')
+def products_show(product_id):
+    product = products.find_one({'_id':ObjectId(product_id)})
+    return render_template('products_show.html', product=product)
+
+
+
+
 
 
 
