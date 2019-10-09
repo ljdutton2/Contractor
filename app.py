07d2808314d4b72610c9fe3e04680931352233c2
lauren <ljdutton2@icloud.com>
2019-10-08 21:18:01 -0700

    .

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ images = [
 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY7bXTlRvTj5RWE1u9sTDE3Y4TVI_YeJ3S07KDIBpGeF4XtFra&s',
 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6pW5rc--yKDwkD0q3xFiI3R0o2NrK9kUg32SHBUuqvbCG6YGw&s'
 ]
+
 plants = [
  {"name": "Spider Plant",
  "picture": images[0],
@@ -36,7 +37,7 @@ plants = [
  {"name": "Assorted Succulents",
  "picture": images[2],
  "price": "1.99",
- "difficulty": "Ez"},  
+ "difficulty": "Easy"},  
  {"name": "Sunflowers",
  "picture": images[3],
  "price": "10.00",
@@ -50,21 +51,20 @@ plants = [
 
 @app.route('/')
 def show_home():
-    return render_template('plants_index.html')
+    return render_template('main_page.html', products=products)
 
 
 @app.route('/view')
 def get_store():
     for plant in plants:
-        products.insert_many(plant)
-    return render_template('all_products.html', products=products.find())
+        products.insert_one(plant)
+        return render_template('all_products.html', products=products.find())
 
 
 @app.route('/products/<product_id>')
 def products_show(product_id):
     product = products.find_one({'_id':ObjectId(product_id)})
     return render_template('products_show.html', product=product)
-
 
 
 
