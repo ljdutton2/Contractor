668d55f3d985c5c8fe93be7cdeac199136b78e36
lauren <ljdutton2@icloud.com>
2019-10-10 16:35:35 -0700

    procfile

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from bson.objectid import ObjectId
 from pymongo import MongoClient
 import os
 from pprint import pprint
+from datetime import datetime
 #from plant_list import Store
 
 app = Flask(__name__)
@@ -30,43 +31,38 @@ def show_home():
             print(x["_id"]) 
         return render_template('all_products.html', product_list=plants)
 
-@app.route('/plants', methods=['GET', 'POST'])
+@app.route('/plants', methods=['POST'])
 def plants_submit():
     """Submit a new plant."""
     plant = {
-        'name': request.form.get('name'),
-        'picture': request.form.get('picture'),
-        'price': request.form.get('price'),
+        'name': request.form.get('plant-name'),
+        'description': request.form.get('plant-description'),
+        'plant-price': request.form.get('plant-price'),
         'difficulty': request.form.get('difficulty'),
-        'created_at': datetime.now()
+        # 'created_at': datetime.now()
     }
     print(plant)
-    plant_id = plant.insert_one(plant).inserted_id
-    return redirect(url_for('new_plant.html', plant_id=plant_id))
+    plant_id = products.insert_one(plant).inserted_id
+    return redirect(url_for('show_home', plant_id=plant_id))
 
 @app.route('/plants/new')
-def playlists_new():
-    """Create a new playlist."""
-    return render_template('new_plant.html',product={})
+def plants_new():
+    return render_template('new_plant.html', plant={})
 
 
-
-@app.route('/products/<product_id>')
-def products_show(product_id):
-    print(product_id)
-    product = products.find_one({'_id':ObjectId(product_id)})
+@app.route('/plants/<plant_id>')
+def products_show(plant_id):
+    print(plant_id)
+    product = products.find_one({'_id': ObjectId(plant_id)})
     print(product)
     return render_template('products_show.html', product=product)
 
-#delete route#
-
-
-
-@app.route('/test', methods=['GET'])
-def test():
-    return "hi"
-
-
+@app.route('/plants/<plant_id>/delete')
+def plants_delete(plant_id):
+    print(plant_id)
+    product = products.delete_one({'_id': ObjectId(plant_id)})
+    print(product)
+    return redirect(url_for('show_home', product=product))
 
 
 if __name__ == '__main__':
